bea03cc27b21e78a3b181456720d21930512241f
DmitriySh <dashishmakov@gmail.com>
2018-07-04 21:48:40 +0300

    Delete import 'junit.framework.*'

--- 02-spring-annotation/src/test/java/ru.shishmakov/service/QuizTest.java
+++ 02-spring-annotation/src/test/java/ru.shishmakov/service/QuizTest.java
@@ -19,9 +19,7 @@ import java.util.Locale;
 
 import static java.lang.Character.UnicodeBlock.*;
 import static java.util.Locale.ENGLISH;
-import static junit.framework.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.*;
 
