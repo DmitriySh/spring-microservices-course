f26983425be0ef092cfe5165b98ca40a44c3bf9d
DmitriySh <dashishmakov@gmail.com>
2018-07-16 20:16:24 +0300

    Fix load ResourceBundle from classpath

--- 03-spring-boot-start/src/main/java/ru/shishmakov/Main.java
+++ 03-spring-boot-start/src/main/java/ru/shishmakov/Main.java
@@ -27,7 +27,7 @@ public class Main {
     @Primary
     public MessageSource messageSource() {
         ReloadableResourceBundleMessageSource ms = new ReloadableResourceBundleMessageSource();
-        ms.setBasename("/i18n/bundle");
+        ms.setBasename("classpath:/i18n/bundle");
         ms.setDefaultEncoding("UTF-8");
         return ms;
     }
