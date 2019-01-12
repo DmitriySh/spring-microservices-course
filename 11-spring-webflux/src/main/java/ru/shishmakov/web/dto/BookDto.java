8045978757d0e4b2f0dc8ea22f418737226ea35f
DmitriySh <dashishmakov@gmail.com>
2019-01-12 15:21:19 +0300

    11: Add ObjectIdSerializer for json mapping

--- 11-spring-webflux/src/main/java/ru/shishmakov/web/dto/BookDto.java
+++ 11-spring-webflux/src/main/java/ru/shishmakov/web/dto/BookDto.java
@@ -2,12 +2,14 @@ package ru.shishmakov.web.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.bson.types.ObjectId;
+import ru.shishmakov.config.ObjectIdSerializer;
 import ru.shishmakov.persistence.entity.Author;
 import ru.shishmakov.persistence.entity.Book;
 import ru.shishmakov.persistence.entity.Genre;
@@ -24,11 +26,14 @@ import static java.util.stream.Collectors.toSet;
 @Setter
 @JsonInclude(Include.NON_EMPTY)
 public class BookDto {
+    @JsonSerialize(using = ObjectIdSerializer.class)
     private ObjectId id;
     private String title;
     private String isbn;
+    @JsonSerialize(contentUsing = ObjectIdSerializer.class)
     @Builder.Default
     private Set<ObjectId> authors = new HashSet<>();
+    @JsonSerialize(contentUsing = ObjectIdSerializer.class)
     @Builder.Default
     private Set<ObjectId> genres = new HashSet<>();
 
